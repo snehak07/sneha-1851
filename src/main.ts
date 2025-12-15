d351cc916f7f0867375fa2ac2d3a58b59033b434
Snehak36 <snehha37@gmail.com>
2025-12-15 09:18:35 +0530

    chore: update project dependencies and bootstrap app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+import { adminSeed } from './database/seed/admin.seed';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,          // removes extra fields
+      forbidNonWhitelisted: true, // throws error on extra fields
+      transform: true,          // auto converts types
+    }),
+  );
+
+  const dataSource = app.get(DataSource);
+  await adminSeed(dataSource);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
