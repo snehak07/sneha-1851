8a77a0bcb5d514f2bdab9592656621729c03e547
Snehak36 <snehha37@gmail.com>
2025-12-18 22:27:37 +0530

    Refactor: updated brand controller

--- src/brands/brand.controller.ts
+++ src/brands/brand.controller.ts
@@ -1,22 +1,31 @@
-import { Controller, Post, Get, Put, Delete, Body, Param, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Req,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Roles } from '../common/decorators/roles.decorator';
 import { RolesGuard } from '../common/guards/roles.guard';
 import { BrandService } from './brand.service';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
-import { UserRole } from '../users/role.enum';
 import { UpdateBrandStatusDto } from './dto/update-brand-status.dto';
-import { ParseIntPipe } from '@nestjs/common';
-
+import { UserRole } from '../users/role.enum';
 
 @UseGuards(AuthGuard('jwt'), RolesGuard)
 @Roles(UserRole.ADMIN)
-@Controller('admin')
+@Controller('admin/brands')
 export class BrandController {
   constructor(private readonly brandService: BrandService) {}
 
-  @Post('brands')
+  @Post()
   create(
     @Body() dto: CreateBrandDto,
     @Req() req,
@@ -24,31 +33,32 @@ export class BrandController {
     return this.brandService.create(dto, req.user);
   }
 
-  @Put('brands/:id/status')
-@Roles(UserRole.ADMIN)
-updateStatus(
-  @Param('id', ParseIntPipe) id: number,
-  @Body() dto: UpdateBrandStatusDto,
-) {
-  return this.brandService.updateStatus(id, dto.status);
-}
+  @Put(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateBrandStatusDto,
+  ) {
+    return this.brandService.updateStatus(id, dto.status);
+  }
 
-
-  @Get('Allbrands')
+  @Get()
   findAll() {
     return this.brandService.findAll();
   }
 
   @Put(':id')
   update(
-    @Param('id') id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateBrandDto,
   ) {
     return this.brandService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number, @Req() req,) {
+  remove(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req,
+  ) {
     return this.brandService.delete(id, req.user);
   }
 }
